fd59ac1cf7b5666131bb83892229e01ec0c91092
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2013-12-07 11:09:37 -0500

    update weapon.h

--- weapon.h
+++ weapon.h
@@ -5,7 +5,6 @@ class Weapon {
         int  m_weight;
         int  m_range;
         int  m_armor_class;
-        char m_type[35]; 
         Weapon(); // default constructor
         Weapon( const char *s, int wt = 0, int rng = 0, int ac = 0 );
         ~Weapon();
