250f92e851266c26d4df249f3578e1fa42725b60
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2013-12-08 00:20:46 -0500

    added weapon.cpp and updated README

---
+++ weapon.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstring>
+#include "weapon.h"
+
+Weapon::Weapon() {
+    std::strcpy( m_weapon_type, "no name" ); 
+    m_weight      = 0;
+    m_range       = 0;
+    m_armor_class = 0;
+}
+
+Weapon::Weapon( const char *wt, int w, int r, int ac ) {
+    std::strncpy( m_weapon_type, wt, 29 ); 
+    m_weapon_type[29] = '\0';
+    m_weight      = w;
+    m_range       = r;
+    m_armor_class = ac;
+}
+
+Weapon::~Weapon() {
+}
+
+void Weapon::show() const {
+    using std::cout;
+    using std::endl;
+    cout << m_weapon_type << ' ' << m_weight << ' ' << m_range << ' ' << m_armor_class << endl;
+}
+
+const Weapon &Weapon::topval( const Weapon &w ) const {
+    if ( w.m_armor_class < m_armor_class ) {
+        return w;
+    } else {
+        return *this;
+    }
+}
