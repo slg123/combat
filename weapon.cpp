4abbc789395e1067806ba905d268f82215d7a9ed
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2013-12-08 20:52:50 -0500

    updated weapon.h and weapon.cpp to use std::string instead of pointer based string

--- weapon.cpp
+++ weapon.cpp
@@ -3,15 +3,14 @@
 #include "weapon.h"
 
 Weapon::Weapon() {
-    std::strcpy( m_weapon_type, "no name" ); 
+    m_weapon_type = "no name"; 
     m_weight      = 0;
     m_range       = 0;
     m_armor_class = 0;
 }
 
 Weapon::Weapon( const char *wt, int w, int r, int ac ) {
-    std::strncpy( m_weapon_type, wt, 29 ); 
-    m_weapon_type[29] = '\0';
+    m_weapon_type = wt; 
     m_weight      = w;
     m_range       = r;
     m_armor_class = ac;
--- weapon.h
+++ weapon.h
@@ -1,12 +1,14 @@
 #ifndef WEAPON_H_
 #define WEAPON_H_
 
+#include <cstring>
+
 class Weapon {
     private:
-        char m_weapon_type[30];
-        int  m_weight;
-        int  m_range;
-        int  m_armor_class;
+        std::string m_weapon_type;
+        int m_weight;
+        int m_range;
+        int m_armor_class;
     public:
         Weapon();
         Weapon( const char *weapon_type, int w = 0, int r = 0, int ac = 0 );
