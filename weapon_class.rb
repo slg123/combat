e2380b5a6dae1dbf17d3bd8e5828b88331d8ead0
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2013-12-10 16:28:10 -0500

    split weapon_class.rb from combat.rb, including weeapon_class.rb in combat.rb listing

---
+++ weapon_class.rb
@@ -0,0 +1,13 @@
+
+class Weapon
+    attr_accessor :type, :weight, :range, :armor_class
+
+    def Weapon.set_attributes( tp="null", wt=0, rg=0, ac=0 )
+        w = Weapon.new
+        w.type        = tp
+        w.weight      = wt
+        w.range       = rg
+        w.armor_class = ac
+        return w
+    end
+end
