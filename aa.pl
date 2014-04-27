69acc2a9531baddcf0da549a7f0308f133cd4537
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2014-04-27 16:01:49 -0400

    update linkedin example

--- aa.pl
+++ aa.pl
@@ -1,17 +1,23 @@
 #!/usr/bin/perl
+
 use strict; 
 use warnings;
 use autodie;
 my $file = "txt";
+my $out  = "txt.out"; 
+
 open my $fh, "<", $file;
+open my $ofh, ">", $out;
 while ( <$fh> ) {
     unless ( /^<\/default/ ) {
         if ( /<default/ ) {
             my ( $begin, $middle, $end ) = split / /; 
             $middle = "enabled";
             print "$begin $middle </default>\n";
+            print $ofh "$begin $middle </default>\n"; 
         } else {
             print;
+            print $ofh $_;
         }
     }
 }
