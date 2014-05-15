1fccbb6d41c83576756aa734a17bcccfd974d314
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2014-05-14 20:51:05 -0400

    update codecademy battleship example

--- battleship.py
+++ battleship.py
@@ -21,8 +21,8 @@ def random_col( board ):
 ship_row = random_row( board )
 ship_col = random_col( board )
 
-print ship_row
-print ship_col
+#print ship_row
+#print ship_col
 
 sank = 0
 counter = 0
