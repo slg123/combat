c200d6e52a2891a16fb88ee841ce554ce2821dc7
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2014-06-01 19:27:36 -0400

    use Socket for ip address reverse name lookup

--- clean.pl
+++ clean.pl
@@ -12,10 +12,7 @@ open my $fh, "<", $file;
 while ( <$fh> ) {
     next if /IP Addr/; 
     my ( $ip, $dash1, $count, $dash2, $dash3, $date ) = split " "; 
-    #my $inet_ip = inet_aton( $ip ); 
-    #my $host = gethostbyaddr( $inet_ip, AF_INET ); 
     my $host = gethostbyaddr( inet_aton( $ip ), AF_INET ); 
-
     if ( $count && $date && $host ) {    
         $seen_ips{ $ip }++ || printf( "%-17s -- %-8d -- %s] -- %s\n", $ip, $count, $date, $host ); 
     }
