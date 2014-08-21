959e6b917b38e7474d53de6f6e880ecdefac1366
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2014-08-20 20:51:29 -0400

    answer to project euler question #2 in Perl

---
+++ fib.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+# By considering the terms in the Fibonacci sequence 
+# whose values do not exceed four million, find the 
+# sum of the even-valued terms.
+
+# http://projecteuler.net/problem=002
+
+use strict;
+use warnings;
+
+my @even_fibs = (); 
+
+sub fibs {
+
+    my $start   = shift;
+    my $upto    = shift;
+    my $curnum  = 1; 
+    my $prevnum = 1;
+    my $sum     = 0;
+
+    while ( $sum < $upto ) {
+        $sum = $curnum + $prevnum;
+        $prevnum = $curnum;
+        $curnum = $sum;
+        if ( $sum % 2 == 0 ) {
+            push @even_fibs, $sum; 
+        }
+    }
+
+}
+
+fibs( 1, 4000000 ); 
+
+our $total = 0; 
+for ( @even_fibs ) {
+    #print " $_ "; 
+    $total += $_; 
+}
+print "total: $total\n";
+
+
