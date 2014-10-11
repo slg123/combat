daccd9bfa7e662904a12c9796df2243556d93592
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2014-10-10 23:04:42 -0400

    projecteuler.net #12 ( brute force ) triangle numbers - find the first triangle number with > 500 divisors

--- tri.c
+++ tri.c
@@ -7,15 +7,15 @@ int main() {
     int k;
     int triangle_num;
 
-    //for ( i = 0; i < 20000; i++ ) {
-    for ( i = 4471; i < 20000; i++ ) {
+    for ( i = 0; i < 20000; i++ ) {
+    //for ( i = 4471; i < 20000; i++ ) {
         k = generate_triangle_number( i ); 
         j = get_factor_count( k ); 
 
         printf( "%d : %d : %d\n", i, k, j ); 
         if ( j > 500 ) {
             printf( "FOUND!! "); 
-            printf( "%d is divisible by > 500 numbers.\n", i ); 
+            printf( "%d is divisible by > 500 numbers.\n", k ); 
             break;
         }
     }
